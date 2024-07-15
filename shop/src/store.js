98e59e5cffdfbbddae96d8d0d3bfd0c6a14b7712
kyinn1307 <shruddls1307@gmail.com>
2024-07-16 02:49:19 +0900

    import something to need from useSlice.js file

--- shop/src/store.js
+++ shop/src/store.js
@@ -1,12 +1,5 @@
-import { configureStore, createSlice } from "@reduxjs/toolkit";
-
-let basket = createSlice({
-  name: "basket",
-  initialState: [
-    { id: 0, name: "White and Black", count: 2 },
-    { id: 2, name: "Grey Yordan", count: 1 },
-  ],
-});
+import { configureStore } from "@reduxjs/toolkit";
+import basket from "./store/useSlice";
 
 export default configureStore({
   reducer: {
