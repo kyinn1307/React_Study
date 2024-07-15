45036395fed598334bb2afa43e50306ee6de5fe8
kyinn1307 <shruddls1307@gmail.com>
2024-07-16 02:50:27 +0900

    add the two buttons with functions named change name and increase

--- shop/src/pages/Cart.js
+++ shop/src/pages/Cart.js
@@ -1,29 +1,53 @@
 import { Table } from "react-bootstrap/";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { changeName } from "../store/useSlice";
+import { increase } from "../store/useSlice";
 
 function Cart() {
   let stock = useSelector((state) => state.basket);
+  let dispatch = useDispatch();
   return (
-    <Table>
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>상품명</th>
-          <th>수량</th>
-        </tr>
-      </thead>
-      <tbody>
-        {stock.map((a, i) => {
-          return (
-            <tr>
-              <td>{stock[i].id}</td>
-              <td>{stock[i].name}</td>
-              <td>{stock[i].count}</td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </Table>
+    <div>
+      <button
+        onClick={() => {
+          dispatch(changeName());
+        }}
+      >
+        버튼
+      </button>
+
+      <Table>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>상품명</th>
+            <th>수량</th>
+            <th>기타</th>
+          </tr>
+        </thead>
+        <tbody>
+          {stock.map((a, i) => {
+            return (
+              <tr>
+                <td>{stock[i].id}</td>
+                <td>{stock[i].name}</td>
+                <td>{stock[i].count}</td>
+                <td>
+                  {" "}
+                  <button
+                    onClick={() => {
+                      dispatch(increase(i));
+                    }}
+                  >
+                    +
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </Table>
+    </div>
   );
 }
 
