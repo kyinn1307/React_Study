c34a6cd535f0aae7d7dbc288a1fc50a72e7e47ac
kyinn1307 <shruddls1307@gmail.com>
2024-07-20 17:30:21 +0900

    study about function of voice, camera

--- animation/src/App.js
+++ animation/src/App.js
@@ -1,26 +1,48 @@
-import "./App.css";
 import styled from "styled-components";
 import React, { useRef, useEffect, useState } from "react";
+import Speech from "./components/Speech";
+
+const Body = styled.div`
+  background-color: black;
+  height: 6000px;
+  font-family: "nanumsquare";
+`;
+const Box = styled.div`
+  padding-top: 1000px;
+  color: white;
+  text-align: center;
+  opacity: ${(props) => props.opacity};
+  transition: all 0.5s;
+`;
+const Video = styled.video`
+  transform: scaleX(-1); /* This will flip the video horizontally */
+`;
+
+const Voice = ({ value, handleInput, handleButton }) => {
+  return (
+    <>
+      <input type="text" onChange={handleInput} value={value}></input>
+      <button onClick={handleButton}>음성 변환</button>
+    </>
+  );
+};
 
 function App() {
-  const Body = styled.div`
-    background-color: black;
-    height: 6000px;
-    font-family: "nanumsquare";
-  `;
-  const Box = styled.div`
-    padding-top: 1000px;
-    color: white;
-    text-align: center;
-  `;
   const videoRef = useRef(null);
   const photoRef = useRef(null);
   const [hasPhoto, setHasPhoto] = useState(false);
-
+  const [value, setValue] = useState("");
+  const [target, setTarget] = useState(false);
+  let sentence = [
+    "iPhone 15 출시",
+    "충전포트를 USB-C 타입으로 바꿔?",
+    "응 바꿨어",
+    "iPhone 15 사진",
+  ];
   // const getVideo = () => {
   //   navigator.mediaDevices
   //     .getUserMedia({
-  //       video: { width: 192, height: 1080 },
+  //       video: { width: 1920, height: 1080 },
   //     })
   //     .then((stream) => {
   //       let video = videoRef.current;
@@ -33,22 +55,36 @@ function App() {
   // };
   // useEffect(() => {
   //   getVideo();
-  // },[videoRef])
+  // }, [videoRef]);
+
+  useEffect(() => {
+    window.speechSynthesis.getVoices();
+  }, []);
+
+  const handleInput = (e) => {
+    setValue(e.target.value);
+    console.log(e.target.value);
+  };
+
+  const handleButton = () => {
+    Speech(value);
+  };
+
   return (
     <Body>
-      <video ref={videoRef}></video>
-      <Box>
-        <h1>iPhone 15 출시</h1>
-      </Box>
-      <Box>
-        <h1>충전포트를 USB-C 타입으로 바꿔?</h1>
-      </Box>
-      <Box>
-        <h1>응 바꿨어</h1>
-      </Box>
-      <Box>
-        <h1>iPhone 15 사진</h1>
-      </Box>
+      {/* <Video ref={videoRef}></Video> */}
+      <Voice
+        value={value}
+        handleInput={handleInput}
+        handleButton={handleButton}
+      />
+      {sentence.map((a) => {
+        return (
+          <Box>
+            <h1>{a}</h1>
+          </Box>
+        );
+      })}
     </Body>
   );
 }
